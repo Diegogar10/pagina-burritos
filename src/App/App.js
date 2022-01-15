dcd543c69d96e1862e4b271388193d8926fdab16
Diego Garcia <diegogar92@gmail.com>
2022-01-15 10:46:43 -0500

    modal de contruccion pagina

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import { ModalContruccion } from '../Pages/ModalContruccion';
+import './App.css';
+
+function App() {
+  return (
+    <ModalContruccion empresa='Tremendo Burrito Colombia'></ModalContruccion>
+  );
+}
+
+export default App;
