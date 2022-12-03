abef99fdd0c9a9b24a088649de593f2b878d3391
Diego Garcia <admin@dydautomatizacion.com>
2022-12-02 20:43:07 -0500

    pruebas con panel description

--- src/Pages/ChooseYourFood.js
+++ src/Pages/ChooseYourFood.js
@@ -1,18 +1,50 @@
 import React from "react";
 import { useMachine } from "@xstate/react";
 import burritoMachine from "../Machines/burroMachine";
+import { PanelDescription } from "../Containers/PanelDescription";
 import './Styles/ArmaTuBurro.scss';
 
 const ChooseYourFood = () => {
 
   const [state, send] = useMachine(burritoMachine);
 
-  
+  const eventContinue = (e) => {
+    state.matches('selectBurro') 
+    ?  send('HOGARENO')
+    :  state.matches('selectProteins1') 
+    ?  send('COMPLETECOUNTS')
+    :  state.matches('defineDislikes') 
+    ?  send('COMPLETECOUNTS')
+    :  state.matches('selectAdditions') 
+    ?  send('COMPLETECOUNTS')
+    :  state.matches('withCombo') 
+    ?  send('YES')
+    :  state.matches('optionAddCart') 
+    ?  send('CONTINUE')
+    :  send('FINISH');
+  }
 
   return(
-    <diV className='armatuburro__container'>
-      
-    </diV>
+    <div className='armatuburro__container'>
+      {
+        state.matches('selectBurro') && <PanelDescription><button onClick={eventContinue}>avanzar1</button></PanelDescription>
+      }
+      {
+        state.matches('selectProteins1') && <PanelDescription><button onClick={eventContinue}>avanzar2</button></PanelDescription>
+      }
+      {
+        state.matches('defineDislikes') && <PanelDescription><button onClick={eventContinue}>avanzar3</button></PanelDescription>
+      }
+      {
+        state.matches('selectAdditions') && <PanelDescription><button onClick={eventContinue}>avanzar4</button></PanelDescription>
+      }
+      {
+        state.matches('withCombo') && <PanelDescription><button onClick={eventContinue}>avanzar5</button></PanelDescription>
+      }
+      {
+        state.matches('optionAddCart') && <PanelDescription><button onClick={eventContinue}>avanzar6</button></PanelDescription>
+      }
+    </div>
   )
 }
 
--- src/containers/PanelDescription.js
+++ src/containers/PanelDescription.js
@@ -1,18 +1,12 @@
 import React from "react";
 import './PanelDescription.scss';
 
-const PanelDescription = ({number, position, classN, url, children}) => {
+const PanelDescription = ({ children}) => {
 
     return(
-        <article className={`armaTuburro__description--${classN} 'visible'`}>
-            <div className={position}>
-                <h4>{number}</h4>
-            </div>
-            <div className="armaTuBurro__content" >
-                {children}
-            </div>
-            <img className={"flecha--"+position} alt='icon' src={url} /> 
-        </article>
+        <section className='general'>
+            { children }
+        </section>
     );
 }
 
